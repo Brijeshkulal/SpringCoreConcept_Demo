df2091c86031c6eb58f874e63164a21c145f9af5
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-12 20:42:01 +0530

    welcome message

---
+++ src/main/java/com/labz/springconcepts/SpringConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.labz.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to the spring concept demo");
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/labz/springconcepts/component/DemoBean.java
@@ -0,0 +1,8 @@
+package com.labz.springconcepts.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoBean {
+
+}

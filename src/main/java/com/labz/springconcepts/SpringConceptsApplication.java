f70e13ad567d678746630220c7be0571a9a6476d
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-12 21:33:08 +0530

    Added logback.xml file

--- src/main/java/com/labz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/labz/springconcepts/SpringConceptsApplication.java
@@ -1,5 +1,7 @@
 package com.labz.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,12 +10,14 @@ import com.labz.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
-		System.out.print("Welcome to spring concept demo");
+		logger.debug("Welcome to spring concept demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean = "+demoBean.toString());
+		logger.warn("Demo Bean = "+demoBean.toString());
 	}
 
 }

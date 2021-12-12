c3ab5a744f4fe4f1b65866f3a8a70808ebf35ecd
“Brijesh” <brijeshkulal5@gmail.com>
2021-12-12 21:03:23 +0530

    created a object of the class using IOc conatiner

--- src/main/java/com/labz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/labz/springconcepts/SpringConceptsApplication.java
@@ -2,13 +2,18 @@ package com.labz.springconcepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.labz.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
-		System.out.print("Welcome to the spring concept demo");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		System.out.print("Welcome to spring concept demo");
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println("Demo Bean = "+demoBean.toString());
 	}
 
 }
